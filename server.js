9c81c38112e14c40a30b627dedb39d87212ca9e9
NikhilDhupar <nikhildhupar207@gmail.com>
2019-04-25 23:07:54 +0530

    Server side code with creating users and login check

---
+++ server.js
@@ -0,0 +1,96 @@
+var express = require('express')
+var path = require('path') 
+var app = express()
+
+//Acces static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Bodyparser
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json()); 
+
+//Connect with db
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost/project1';
+
+mongoose.connect(mongoDB);
+
+mongoose.connection.on('error', (err) => {
+    console.log('DB connection Error');
+});
+
+mongoose.connection.on('connected', (err) => {
+    console.log('DB connected');
+});
+
+var productSchema = new mongoose.Schema({
+    name: String,
+    email : String,
+    gender: String,
+    password : String,
+    city : String,
+    phno : Number,
+    dob : String,
+    //category : [{ 'abd': Number , }]
+  });
+
+var user =  mongoose.model('users', productSchema);
+
+app.get('/',function(req,res){
+  res.redirect('login.html')
+    user.find({})
+    .then(data => {
+        console.log(data)
+        res.send(data)
+      })
+      .catch(err => {
+        console.error(err)
+        res.send(error)
+      })
+})
+app.post('/login',function(req,res){
+  console.log(req.body);
+  user.find({"name":req.body.name,"password":req.body.password})
+
+  .then(data => {
+    if(data.length!=0)
+    {
+      console.log(data)
+      res.send("1");
+    }
+    else
+    {
+      res.send("0");
+    }
+  })
+  .catch(err => {
+    console.error(err)
+    res.send(err);
+    //res.send("Invalid Email/password")
+  })
+})
+
+app.post('/adduser',function(req,res){
+  console.log(req.body);
+    let newuser = new user({
+      name: req.body.name,
+      email : req.body.email,
+      gender: req.body.gender,
+      password : req.body.password,
+      city : req.body.city,
+      phno : req.body.phone,
+      dob : req.body.dob,
+    })
+    newuser.save()
+     .then(data => {
+       console.log(data)
+       res.send(data)
+     })
+     .catch(err => {
+       console.error(err)
+       res.send(error)
+     })
+})
+
+console.log("Running on port 3000");
+app.listen(3000)
